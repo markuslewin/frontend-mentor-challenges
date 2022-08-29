9f1e712ae8b300db27ec8a0d652938cc8bf86918
Markus Lewin <lewinmarkus@gmail.com>
2022-08-29 19:30:23 +0200

    Add styles

--- product-preview-card-component/tailwind.config.js
+++ product-preview-card-component/tailwind.config.js
@@ -1,50 +1,77 @@
+const plugin = require("tailwindcss/plugin");
+
 function pxToRem(px) {
   return `${px / 16}rem`;
 }
 
-const colors = {
-  "pure-white": "hsl(0, 0%, 100%)",
-  cream: "hsl(30, 38%, 92%)",
-  "aurometal-saurus": "hsl(228, 12%, 48%)",
-  "deep-aquamarine": "hsl(158, 36%, 37%)",
-  "deep-aquamarine-hover": "hsl(157, 43%, 18%)",
-  gunmetal: "hsl(212, 21%, 14%)",
-};
-
-const fontFamily = {
-  fraunces: "Fraunces, serif",
-  montserrat: "Montserrat, sans-serif",
-};
-
-const fontSize = {
-  large: pxToRem(32),
-  medium: pxToRem(14),
-  small: pxToRem(13),
-  "extra-small": pxToRem(12),
-};
-
-const fontWeight = {
-  normal: 500,
-  bold: 700,
-};
-
-const lineHeight = {
-  large: pxToRem(32),
-  medium: pxToRem(23),
-  small: pxToRem(17),
-  "extra-small": pxToRem(15),
-};
-
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    colors,
-    fontFamily,
-    fontSize,
-    fontWeight,
-    lineHeight,
+    colors: {
+      "pure-white": "hsl(0, 0%, 100%)",
+      cream: "hsl(30, 38%, 92%)",
+      "aurometal-saurus": "hsl(228, 12%, 48%)",
+      "deep-aquamarine": "hsl(158, 36%, 37%)",
+      "deep-aquamarine-hover": "hsl(157, 43%, 18%)",
+      gunmetal: "hsl(212, 21%, 14%)",
+    },
+    fontFamily: {
+      fraunces: "Fraunces, serif",
+      montserrat: "Montserrat, sans-serif",
+    },
+    fontSize: {
+      large: pxToRem(32),
+      medium: pxToRem(14),
+      small: pxToRem(13),
+      "extra-small": pxToRem(12),
+    },
+    fontWeight: {
+      medium: 500,
+      bold: 700,
+    },
+    lineHeight: {
+      large: pxToRem(32),
+      medium: pxToRem(23),
+      small: pxToRem(17),
+      "extra-small": pxToRem(15),
+    },
+    borderRadius: {
+      small: 8,
+      large: 10,
+    },
     extend: {},
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require("tailwindcss-logical"),
+    plugin(function ({ addUtilities, theme }) {
+      addUtilities({
+        ".text-style-display": {
+          fontFamily: theme("fontFamily.fraunces"),
+          fontWeight: theme("fontWeight.bold"),
+          fontSize: theme("fontSize.large"),
+          lineHeight: theme("lineHeight.large"),
+        },
+        ".text-style-body": {
+          fontFamily: theme("fontFamily.montserrat"),
+          fontWeight: theme("fontWeight.medium"),
+          fontSize: theme("fontSize.medium"),
+          lineHeight: theme("lineHeight.medium"),
+        },
+        ".text-style-button-text": {
+          fontFamily: theme("fontFamily.montserrat"),
+          fontWeight: theme("fontWeight.bold"),
+          fontSize: theme("fontSize.medium"),
+          lineHeight: theme("lineHeight.small"),
+        },
+        ".text-style-overline": {
+          fontFamily: theme("fontFamily.montserrat"),
+          fontWeight: theme("fontWeight.medium"),
+          fontSize: theme("fontSize.extra-small"),
+          lineHeight: theme("lineHeight.extra-small"),
+          letterSpacing: pxToRem(5),
+        },
+      });
+    }),
+  ],
 };
