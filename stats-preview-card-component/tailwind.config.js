ce0f304e6aef248bba0feca4f34a1fb979e72bd4
Markus Lewin <lewinmarkus@gmail.com>
2022-09-23 22:02:56 +0200

    Add base styles

--- stats-preview-card-component/tailwind.config.js
+++ stats-preview-card-component/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 const pxToRem = require("./css-utils/px-to-rem");
 const tokensToTailwind = require("./css-utils/tokens-to-tailwind");
 const hasVariableFont = require("./css-utils/has-variable-font");
@@ -34,5 +36,31 @@ module.exports = {
     extend: {},
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    plugin(({ addComponents, theme }) => {
+      const prefixByProperty = {
+        colors: "color",
+        fontSize: "size",
+      };
+
+      for (const [property, prefix] of Object.entries(prefixByProperty)) {
+        addComponents({
+          ":root": Object.fromEntries(
+            Object.entries(theme(property)).map(([name, value]) => [
+              `--${prefix}-${name}`,
+              value,
+            ])
+          ),
+        });
+      }
+      addComponents({
+        ":root": Object.fromEntries(
+          Object.entries(theme("fontFamily")).map(([name, value]) => [
+            `--font-${name}`,
+            value.join(", "),
+          ])
+        ),
+      });
+    }),
+  ],
 };
