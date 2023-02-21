78080902996905a0c18a527afe529fc1ef9615ed
Markus Lewin <lewinmarkus@gmail.com>
2023-02-21 13:15:55 +0100

    Remove boilerplate

--- results-summary-component/app/routes/index.tsx
+++ results-summary-component/app/routes/index.tsx
@@ -21,13 +21,6 @@ export default function Index() {
       Your Result 76 of 100 Great You scored higher than 65% of the people who
       have taken these tests. Summary Reaction 80 / 100 Memory 92 / 100 Verbal
       61 / 100 Visual 72 / 100 Continue
-      <div className="attribution">
-        Challenge by
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
     </>
   );
 }
