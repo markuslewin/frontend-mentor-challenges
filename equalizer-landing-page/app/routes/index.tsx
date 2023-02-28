eb238579b29e7273bf4e03b7352b5b071a94f36b
Markus Lewin <lewinmarkus@gmail.com>
2023-02-28 19:41:13 +0100

    Structure document

--- equalizer-landing-page/app/routes/index.tsx
+++ equalizer-landing-page/app/routes/index.tsx
@@ -1,27 +1,83 @@
 export default function Index() {
   return (
-    <main>
-      <h1>Equalizer</h1>
-      <pre>
-        {`We make your music sound extraordinary.
-
-A system audio equalizer specifically designed for Android and iOS. Freely tune the 
-way your music sounds with a professional grade parametric EQ & volume mixer. Control 
-bass, mids, treble, gain control, reverb, and more!
-
-Premium EQ
-
-Get expert-level control with a robust equalizer, volume mixer, and spatial audio. Take 
-your listening experience to a whole new level and access all our incredible features!
-
-$4 / month
-
-iOS Download
-Android Download
-
-All rights reserved © Equalizer 2021
-Have any problems? Contact us via social media or email us at equalizer@example.com`}
-      </pre>
-    </main>
+    <>
+      <header>
+        <img alt="equalizer" width="147" height="33" src="assets/logo.svg" />
+      </header>
+      <main>
+        <article>
+          <h1>We make your music sound extraordinary.</h1>
+          <p>
+            A system audio equalizer specifically designed for Android and iOS.
+            Freely tune the way your music sounds with a professional grade
+            parametric EQ & volume mixer. Control bass, mids, treble, gain
+            control, reverb, and more!
+          </p>
+        </article>
+        <article>
+          <img
+            alt="the equalizer user interface"
+            src="assets/illustration-app.png"
+          />
+          <article>
+            <h2>Premium EQ</h2>
+            <p>
+              Get expert-level control with a robust equalizer, volume mixer,
+              and spatial audio. Take your listening experience to a whole new
+              level and access all our incredible features!
+            </p>
+            <p>
+              <strong>$4</strong> / month
+            </p>
+            <h3>Download links</h3>
+            <ul>
+              {[
+                { icon: "apple", width: 18, label: "iOS Download" },
+                { icon: "android", width: 17, label: "Android Download" },
+              ].map((download, index) => (
+                <li key={index}>
+                  <a href="#">
+                    <img
+                      alt=""
+                      width={download.width}
+                      height="20"
+                      src={`assets/icon-${download.icon}.svg`}
+                    />{" "}
+                    {download.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </article>
+        </article>
+      </main>
+      <footer>
+        <img alt="equalizer" width="147" height="33" src="assets/logo.svg" />
+        <p>All rights reserved © Equalizer 2021</p>
+        <p>
+          Have any problems? Contact us via social media or email us at{" "}
+          <a href="mailto:equalizer@example.com">equalizer@example.com</a>
+        </p>
+        <p>Social media links:</p>
+        <ul>
+          {[
+            { name: "facebook" },
+            { name: "instagram" },
+            { name: "twitter", height: 17 },
+          ].map((site, index) => (
+            <li key={index}>
+              <a href="#">
+                <img
+                  alt={site.name}
+                  width="20"
+                  height={site.height ?? 20}
+                  src={`assets/icon-${site.name}.svg`}
+                />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    </>
   );
 }
