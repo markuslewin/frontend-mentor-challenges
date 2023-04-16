de0f3c478de679f8100df8feb2e550a5e6f99408
Markus Lewin <lewinmarkus@gmail.com>
2023-04-16 19:07:21 +0200

    Add base styles

--- advice-generator-app/tailwind.config.cjs
+++ advice-generator-app/tailwind.config.cjs
@@ -1,8 +1,39 @@
+const rem = (px) => `${px / 16}rem`;
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: { font: ["ManropeVariable", "sans-serif"] },
+    fontWeight: {
+      "extra-bold": 800,
+    },
+    fontSize: {
+      "heading-mobile": rem(11),
+      "quote-mobile": rem(24),
+      "heading-desktop": rem(13),
+      "quote-desktop": rem(28),
+    },
+    letterSpacing: {
+      "heading-mobile": rem(3.46),
+      "quote-mobile": rem(-0.26),
+      "heading-desktop": rem(4.09),
+      "quote-desktop": rem(-0.3),
+    },
+    lineHeight: {
+      "heading-mobile": rem(15),
+      "quote-mobile": rem(33),
+      "heading-desktop": rem(18),
+      "quote-desktop": rem(38),
+    },
+    colors: {
+      "dark-blue": "hsl(218, 23%, 16%)",
+      "dark-grayish-blue": "hsl(217, 19%, 24%)",
+      "grayish-blue": "hsl(217, 19%, 38%)",
+      "light-cyan": "hsl(193, 38%, 86%)",
+      "neon-green": "hsl(150, 100%, 66%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
