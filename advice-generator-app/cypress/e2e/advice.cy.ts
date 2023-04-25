019983e3f8f047c01c1f1987c9989273df2042be
Markus Lewin <lewinmarkus@gmail.com>
2023-04-25 23:07:48 +0200

    Add tests

--- advice-generator-app/cypress/e2e/advice.cy.ts
+++ advice-generator-app/cypress/e2e/advice.cy.ts
@@ -1,5 +1,31 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("advice", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("matches the design initially", () => {
+    cy.findByRole("heading", { name: "Advice #117" });
+    cy.findByText(
+      "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+    );
+  });
+
+  it("generates new advice on button click", () => {
+    const id = 123;
+    const advice = "some advice";
+
+    cy.intercept("GET", "https://api.adviceslip.com/advice", {
+      statusCode: 200,
+      body: {
+        slip: {
+          id,
+          advice,
+        },
+      },
+    });
+
+    cy.findByRole("button", { name: /new/i }).click();
+    cy.findByRole("heading", { name: new RegExp(id.toString()) });
+    cy.findByText(advice);
   });
 });
