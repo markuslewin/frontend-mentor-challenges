95af1b7aa5b252b5d411cc4a16acafd981a842dd
Markus Lewin <lewinmarkus@gmail.com>
2023-04-27 18:00:35 +0200

    Add netlify adapter

--- advice-generator-app/astro.config.mjs
+++ advice-generator-app/astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [
     tailwind({
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
   ],
+  adapter: netlify(),
 });
