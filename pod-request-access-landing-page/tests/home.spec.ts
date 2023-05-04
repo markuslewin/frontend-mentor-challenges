df944664c7f9a2a9036e0d1466ae1a616aa8002f
Markus Lewin <lewinmarkus@gmail.com>
2023-05-04 19:21:30 +0200

    Add requirements

--- pod-request-access-landing-page/tests/home.spec.ts
+++ pod-request-access-landing-page/tests/home.spec.ts
@@ -1,8 +1,71 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("/");
+});
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Pod request access landing page/i);
+const invalidEmail = "abc";
+const messages = {
+  empty: "Oops! Please add your email",
+  notFormattedCorrectly: "Oops! Please check your email",
+};
+
+test.describe("enhanced", () => {
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle(/pod request access landing page/i);
+  });
+
+  test("displays the correct error message when empty", async ({ page }) => {
+    const input = page.getByLabel("email");
+    await expect(input).toBeEmpty();
+
+    page.getByRole("button", { name: /request access/i }).click();
+
+    await expect(page.getByText(messages.empty)).toBeAttached();
+    await expect(input).toHaveAttribute("aria-invalid", "true");
+    await expect(input).toBeFocused();
+  });
+
+  test("displays the correct error message when not formatted correctly", async ({
+    page,
+  }) => {
+    const input = page.getByLabel("email");
+
+    input.fill(invalidEmail);
+    input.press("Enter");
+
+    await expect(page.getByText(messages.notFormattedCorrectly)).toBeAttached();
+    await expect(input).toHaveAttribute("aria-invalid", "true");
+    await expect(input).toBeFocused();
+  });
+});
+
+test.describe("no javascript", () => {
+  test.use({ javaScriptEnabled: false });
+
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle(/pod request access landing page/i);
+  });
+
+  test("displays the correct error message when empty", async ({ page }) => {
+    const input = page.getByLabel("email");
+    await expect(input).toBeEmpty();
+
+    page.getByRole("button", { name: /request access/i }).click();
+
+    await expect(page.getByText(messages.empty)).toBeAttached();
+    await expect(input).toHaveAttribute("aria-invalid", "true");
+  });
+
+  test("displays the correct error message when not formatted correctly", async ({
+    page,
+  }) => {
+    const input = page.getByLabel("email");
+
+    input.fill(invalidEmail);
+    input.press("Enter");
+
+    await expect(page.getByText(messages.notFormattedCorrectly)).toBeAttached();
+    await expect(input).toHaveAttribute("aria-invalid", "true");
+  });
 });
