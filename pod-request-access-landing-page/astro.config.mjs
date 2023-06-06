d46219b497b653fdf6ffcd0a556ca8a08ae9fd05
Markus Lewin <lewinmarkus@gmail.com>
2023-06-06 18:13:17 +0200

    Add netlify adapter

--- pod-request-access-landing-page/astro.config.mjs
+++ pod-request-access-landing-page/astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind({ config: { applyBaseStyles: false } })],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
+  adapter: netlify(),
 });
