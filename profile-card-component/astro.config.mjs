b9d38ed5543520bec3a8fab3b7cd649397606ac5
Markus Lewin <lewinmarkus@gmail.com>
2023-06-08 20:31:32 +0200

    Add astro

---
+++ profile-card-component/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
+
+export default defineConfig({
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+});
