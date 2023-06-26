c5aa1cda2b069fcc374585b73235f9f0065d7882
Markus Lewin <lewinmarkus@gmail.com>
2023-06-26 14:10:13 +0200

    Switch to remix

---
+++ intro-component-with-signup-form/app/root.tsx
@@ -0,0 +1,112 @@
+import {
+  Link,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useCatch,
+} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+
+import globalStylesUrl from "~/styles/global.css";
+
+// https://remix.run/api/app#links
+export let links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: globalStylesUrl }];
+};
+
+// https://remix.run/api/conventions#default-export
+// https://remix.run/api/conventions#route-filenames
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+// https://remix.run/docs/en/v1/api/conventions#errorboundary
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+  return (
+    <Document title="Error!">
+      <div>
+        <h1>There was an error</h1>
+        <p>{error.message}</p>
+        <hr />
+        <p>
+          Hey, developer, you should replace this with what you want your users
+          to see.
+        </p>
+      </div>
+    </Document>
+  );
+}
+
+// https://remix.run/docs/en/v1/api/conventions#catchboundary
+export function CatchBoundary() {
+  let caught = useCatch();
+
+  let message;
+  switch (caught.status) {
+    case 401:
+      message = (
+        <p>
+          Oops! Looks like you tried to visit a page that you do not have access
+          to.
+        </p>
+      );
+      break;
+    case 404:
+      message = (
+        <p>Oops! Looks like you tried to visit a page that does not exist.</p>
+      );
+      break;
+
+    default:
+      throw new Error(caught.data || caught.statusText);
+  }
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <h1>
+        {caught.status}: {caught.statusText}
+      </h1>
+      {message}
+    </Document>
+  );
+}
+
+function Document({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="./images/favicon-32x32.png"
+        />
+        <title>Frontend Mentor | Intro component with sign up form</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
---
+++ intro-component-with-signup-form/app/routes/index.tsx
@@ -0,0 +1,140 @@
+import { Form, useActionData } from "@remix-run/react";
+import { type ActionArgs, json, redirect } from "@remix-run/node";
+import { useId } from "react";
+import { useForm } from "@conform-to/react";
+import { parseRegisterForm } from "~/helpers/validation";
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+  const submission = parseRegisterForm(formData);
+
+  if (submission.intent !== "submit" || !submission.value) {
+    return json({
+      ...submission,
+      payload: {
+        "first-name": submission.payload["first-name"],
+        "last-name": submission.payload["last-name"],
+        "email-address": submission.payload["email-address"],
+        // no password
+      },
+    });
+  }
+
+  console.log("success!");
+
+  // todo: persist
+  return redirect("/");
+};
+
+export default function Index() {
+  const lastSubmission = useActionData<typeof action>();
+  const id = useId();
+  const [
+    form,
+    {
+      "first-name": firstName,
+      "last-name": lastName,
+      "email-address": emailAddress,
+      password,
+    },
+  ] = useForm({
+    id,
+    lastSubmission,
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+    onValidate({ formData }) {
+      return parseRegisterForm(formData);
+    },
+  });
+
+  return (
+    <main>
+      <div className="intro">
+        <h1>Learn to code by watching others</h1>
+        <p>
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
+        </p>
+      </div>
+      <div className="register">
+        <h2 className="sr-only">Claim your free trial</h2>
+        <p className="pricing">
+          <strong>Try it free 7 days</strong> then $20/mo. thereafter
+        </p>
+        <Form method="post" {...form.props}>
+          <label className="sr-only" htmlFor={firstName.id}>
+            First Name
+          </label>
+          <input
+            id={firstName.id}
+            type="text"
+            name={firstName.name}
+            autoComplete="given-name"
+            required
+            placeholder="First Name"
+            defaultValue={firstName.defaultValue}
+            autoFocus={!!firstName.error}
+            aria-describedby={firstName.errorId}
+            aria-invalid={!!firstName.error}
+          />
+          <p id={firstName.errorId}>{firstName.error}</p>
+          <label className="sr-only" htmlFor={lastName.id}>
+            Last Name
+          </label>
+          <input
+            id={lastName.id}
+            type="text"
+            name={lastName.name}
+            autoComplete="family-name"
+            required
+            placeholder="Last Name"
+            defaultValue={lastName.defaultValue}
+            autoFocus={!!lastName.error}
+            aria-describedby={lastName.errorId}
+            aria-invalid={!!lastName.error}
+          />
+          <p id={lastName.errorId}>{lastName.error}</p>
+          <label className="sr-only" htmlFor={emailAddress.id}>
+            Email Address
+          </label>
+          <input
+            id={emailAddress.id}
+            type="email"
+            name={emailAddress.name}
+            autoComplete="email"
+            required
+            placeholder="Email Address"
+            defaultValue={emailAddress.defaultValue}
+            autoFocus={!!emailAddress.error}
+            aria-describedby={emailAddress.errorId}
+            aria-invalid={!!emailAddress.error}
+          />
+          <p id={emailAddress.errorId}>{emailAddress.error}</p>
+          <label className="sr-only" htmlFor={password.id}>
+            Password
+          </label>
+          <input
+            id={password.id}
+            type="password"
+            name={password.name}
+            autoComplete="new-password"
+            required
+            placeholder="Password"
+            autoFocus={!!password.error}
+            aria-describedby={password.errorId}
+            aria-invalid={!!password.error}
+          />
+          <p id={password.errorId}>{password.error}</p>
+          <button type="submit" aria-describedby="agreement">
+            Claim your free trial
+          </button>
+          <small id="agreement">
+            By clicking the button, you are agreeing to our{" "}
+            <a href="#">Terms and Services</a>
+          </small>
+        </Form>
+      </div>
+    </main>
+  );
+}
