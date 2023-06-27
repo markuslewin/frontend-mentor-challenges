a10bbfc3477de4166998ac99c6be6f003201137e
Markus Lewin <lewinmarkus@gmail.com>
2023-06-27 20:11:55 +0200

    Fix svg error

--- intro-component-with-signup-form/app/components/Icon.tsx
+++ intro-component-with-signup-form/app/components/Icon.tsx
@@ -7,7 +7,7 @@ export const ErrorIcon = () => {
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g fill="none" fill-rule="evenodd">
+      <g fill="none" fillRule="evenodd">
         <circle fill="#FF7979" cx="12" cy="12" r="12" />
         <rect fill="#FFF" x="11" y="6" width="2" height="9" rx="1" />
         <rect fill="#FFF" x="11" y="17" width="2" height="2" rx="1" />
