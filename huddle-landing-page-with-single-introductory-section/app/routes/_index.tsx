d8c0f137c5bd77856cf92fcf650837677f4ee610
Markus Lewin <lewinmarkus@gmail.com>
2023-07-05 11:31:04 +0200

    Style page

--- huddle-landing-page-with-single-introductory-section/app/routes/_index.tsx
+++ huddle-landing-page-with-single-introductory-section/app/routes/_index.tsx
@@ -1,30 +1,41 @@
 import { Facebook, Instagram, Twitter } from "../components/icon";
+import { Huddle } from "../components/logo";
 
 export default function Index() {
   return (
     <>
-      <header>
-        <img alt="huddle" src="/images/logo.svg" />
+      <header className="center">
+        <Huddle /> <span className="sr-only">Huddle</span>
       </header>
-      <main>
-        <img alt="" src="/images/illustration-mockups.svg" />
-        <h1>Build The Community Your Fans Will Love</h1>
-        <p>
-          Huddle re-imagines the way we build communities. You have a voice, but
-          so does your audience. Create connections with your users as you
-          engage in genuine discussion.
-        </p>
-        <p>
-          <a className="button" href="#">
-            Register
-          </a>
-        </p>
+      <main className="[ center ] [ mt-16 md:mt-28 ]">
+        <img
+          className="mx-auto"
+          alt=""
+          src="/images/illustration-mockups.svg"
+        />
+        <div className="mt-16">
+          <h1>Build The Community Your Fans Will Love</h1>
+          <p className="mt-4 md:mt-6">
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genuine discussion.
+          </p>
+          <p className="mt-6">
+            <a className="button" href="#">
+              Register
+            </a>
+          </p>
+        </div>
       </main>
-      <footer>
+      <footer className="[ center ] [ mt-16 md:mt-7 ]">
         <h2 id="socials-heading" className="sr-only">
           Social media links
         </h2>
-        <ul role="list" aria-describedby="socials-heading">
+        <ul
+          className="flex flex-wrap justify-center gap-3 md:gap-4 lg:justify-end"
+          role="list"
+          aria-describedby="socials-heading"
+        >
           {[
             { name: "facebook", icon: <Facebook /> },
             { name: "twitter", icon: <Twitter /> },
