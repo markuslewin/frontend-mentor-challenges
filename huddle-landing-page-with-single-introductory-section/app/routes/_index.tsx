dc0dc49e81641f8279b4750a164788919cc10b5f
Markus Lewin <lewinmarkus@gmail.com>
2023-07-05 15:17:52 +0200

    Switch to tailwind breakpoints

--- huddle-landing-page-with-single-introductory-section/app/routes/_index.tsx
+++ huddle-landing-page-with-single-introductory-section/app/routes/_index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
           alt=""
           src="/images/illustration-mockups.svg"
         />
-        <div className="mt-16 md:mt-8">
+        <div className="mt-16 lg:mt-8">
           <h1>Build The Community Your Fans Will Love</h1>
           <p className="mt-4 md:mt-6">
             Huddle re-imagines the way we build communities. You have a voice,
