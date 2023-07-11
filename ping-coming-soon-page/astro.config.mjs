f375307a5d86f6f1ceed66fbefcd60073c8cb238
Markus Lewin <lewinmarkus@gmail.com>
2023-07-11 17:48:49 +0200

    Add netlify adapter

--- ping-coming-soon-page/astro.config.mjs
+++ ping-coming-soon-page/astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  })],
+  adapter: netlify()
 });
