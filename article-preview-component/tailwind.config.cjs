44202c2572a46262b9ac1fd7860187f8779d9c06
Markus Lewin <lewinmarkus@gmail.com>
2023-07-26 14:18:05 +0200

    Disable floating ui for mobile breakpoint

--- article-preview-component/tailwind.config.cjs
+++ article-preview-component/tailwind.config.cjs
@@ -1,11 +1,10 @@
+const { screens } = require("./src/utils/screens");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    screens: {
-      tablet: "40em",
-      desktop: "64em",
-    },
+    screens,
     extend: {},
   },
   plugins: [],
