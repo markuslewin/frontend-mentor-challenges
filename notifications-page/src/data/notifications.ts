b887184b29f10a24f405231efe4c219f9a3d84c5
Markus Lewin <lewinmarkus@gmail.com>
2023-08-25 16:16:11 +0200

    Fix datetimes

--- notifications-page/src/data/notifications.ts
+++ notifications-page/src/data/notifications.ts
@@ -1,3 +1,5 @@
+const now = Date.now();
+
 export const notifications = [
   {
     type: "reacted" as const,
@@ -6,7 +8,10 @@ export const notifications = [
       name: "Mark Webber",
       avatar: "/assets/images/avatar-mark-webber.webp",
     },
-    time: { datetime: "", formatted: "1m ago" },
+    time: {
+      datetime: new Date(now - 1000 * 60).toISOString(),
+      formatted: "1m ago",
+    },
     post: "My first tournament today",
   },
   {
@@ -16,7 +21,10 @@ export const notifications = [
       name: "Angela Gray",
       avatar: "/assets/images/avatar-angela-gray.webp",
     },
-    time: { datetime: "", formatted: "5m ago" },
+    time: {
+      datetime: new Date(now - 1000 * 60 * 5).toISOString(),
+      formatted: "5m ago",
+    },
   },
   {
     type: "joined" as const,
@@ -25,7 +33,10 @@ export const notifications = [
       name: "Jacob Thompson",
       avatar: "/assets/images/avatar-jacob-thompson.webp",
     },
-    time: { datetime: "", formatted: "1 day ago" },
+    time: {
+      datetime: new Date(now - 1000 * 60 * 60 * 24).toISOString(),
+      formatted: "1 day ago",
+    },
     group: "Chess Club",
   },
   {
@@ -35,7 +46,10 @@ export const notifications = [
       name: "Rizky Hasanuddin",
       avatar: "/assets/images/avatar-rizky-hasanuddin.webp",
     },
-    time: { datetime: "", formatted: "5 days ago" },
+    time: {
+      datetime: new Date(now - 1000 * 60 * 60 * 24 * 5).toISOString(),
+      formatted: "5 days ago",
+    },
     message:
       "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
   },
@@ -46,7 +60,10 @@ export const notifications = [
       name: "Kimberly Smith",
       avatar: "/assets/images/avatar-kimberly-smith.webp",
     },
-    time: { datetime: "", formatted: "1 week ago" },
+    time: {
+      datetime: new Date(now - 1000 * 60 * 60 * 24 * 7).toISOString(),
+      formatted: "1 week ago",
+    },
     picture: "/assets/images/image-chess.webp",
   },
   {
@@ -56,7 +73,10 @@ export const notifications = [
       name: "Nathan Peterson",
       avatar: "/assets/images/avatar-nathan-peterson.webp",
     },
-    time: { datetime: "", formatted: "2 weeks ago" },
+    time: {
+      datetime: new Date(now - 1000 * 60 * 60 * 24 * 7 * 2).toISOString(),
+      formatted: "2 weeks ago",
+    },
     post: "5 end-game strategies to increase your win rate",
   },
   {
@@ -66,7 +86,10 @@ export const notifications = [
       name: "Anna Kim",
       avatar: "/assets/images/avatar-anna-kim.webp",
     },
-    time: { datetime: "", formatted: "2 weeks ago" },
+    time: {
+      datetime: new Date(now - 1000 * 60 * 60 * 24 * 7 * 2).toISOString(),
+      formatted: "2 weeks ago",
+    },
     group: "Chess Club",
   },
 ];
