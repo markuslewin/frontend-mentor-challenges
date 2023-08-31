e105cfb944d9b0d6704cee9340d3ee7f665c9bf4
Markus Lewin <lewinmarkus@gmail.com>
2023-08-31 19:11:20 +0200

    Add markup

---
+++ single-page-design-portfolio/src/data/slides.ts
@@ -0,0 +1,37 @@
+export const slides = [
+  {
+    alt: "alternative text 1",
+    caption: "caption 1",
+    src: "/assets/image-slide-1.jpg",
+    width: 540,
+    height: 360,
+  },
+  {
+    alt: "alternative text 2",
+    caption: "caption 2",
+    src: "/assets/image-slide-2.jpg",
+    width: 540,
+    height: 360,
+  },
+  {
+    alt: "alternative text 3",
+    caption: "caption 3",
+    src: "/assets/image-slide-3.jpg",
+    width: 540,
+    height: 360,
+  },
+  {
+    alt: "alternative text 4",
+    caption: "caption 4",
+    src: "/assets/image-slide-4.jpg",
+    width: 540,
+    height: 360,
+  },
+  {
+    alt: "alternative text 5",
+    caption: "caption 5",
+    src: "/assets/image-slide-5.jpg",
+    width: 540,
+    height: 360,
+  },
+];
