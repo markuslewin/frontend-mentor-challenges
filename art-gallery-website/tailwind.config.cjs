8a663b592eb5870a17a4ee7ef361433e7dab5b09
Markus Lewin <lewinmarkus@gmail.com>
2023-09-22 21:31:42 +0200

    Apply font styles

--- art-gallery-website/tailwind.config.cjs
+++ art-gallery-website/tailwind.config.cjs
@@ -1,8 +1,11 @@
+const { screens } = require("./src/utils/screens");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens,
+    extend: {},
+  },
+  plugins: [],
+};
