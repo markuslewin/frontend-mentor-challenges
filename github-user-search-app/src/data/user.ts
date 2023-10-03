376020c5bd7ab200d41efdaf61baf7f518525ba1
Markus Lewin <lewinmarkus@gmail.com>
2023-10-03 14:34:48 +0200

    Add search

---
+++ github-user-search-app/src/data/user.ts
@@ -0,0 +1,36 @@
+import { Octokit, RequestError } from "octokit";
+import userData from "../data/user.json";
+
+const parse = (formData: FormData) => {
+  const username = formData.get("username");
+  if (typeof username !== "string") {
+    return { error: "`username` is not a string" };
+  }
+  return { data: { username } };
+};
+
+const octokit = new Octokit();
+const getUser = async (username: string) => {
+  // todo: Remove
+  if (import.meta.env.DEV) {
+    const user = {
+      ...userData,
+      bio: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.",
+    };
+    return { data: user };
+  }
+
+  try {
+    const response = await octokit.rest.users.getByUsername({
+      username,
+    });
+    return { data: response.data };
+  } catch (error) {
+    if (error instanceof RequestError) {
+      return { error };
+    }
+    throw error;
+  }
+};
+
+export { parse, getUser };
