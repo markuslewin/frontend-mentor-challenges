0d078ef5fe0487f70c87efe122ccd0451608a9af
Markus Lewin <lewinmarkus@gmail.com>
2023-10-04 20:56:38 +0200

    Add netlify adapter

--- github-user-search-app/astro.config.mjs
+++ github-user-search-app/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [preact()]
+  integrations: [preact()],
+  adapter: netlify()
 });
