0e1030f2cb390bd1bb514abd485f2d0caf12ff41
Markus Lewin <lewinmarkus@gmail.com>
2023-10-04 20:15:35 +0200

    Add preact

--- github-user-search-app/astro.config.mjs
+++ github-user-search-app/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  integrations: [preact()]
 });
