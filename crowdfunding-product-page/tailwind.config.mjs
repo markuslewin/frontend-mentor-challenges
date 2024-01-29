6344def1f350f87da6d338a5f0840b4a466e0d6c
Markus Lewin <lewinmarkus@gmail.com>
2024-01-29 22:27:51 +0100

    Add tailwind

---
+++ crowdfunding-product-page/tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        tablet: "40em",
+        desktop: "64em",
+      },
+    },
+  },
+  plugins: [],
+};
