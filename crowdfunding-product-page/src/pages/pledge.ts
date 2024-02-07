e6d95811a2588029bd129b4feff69909a7d2c7f4
Markus Lewin <lewinmarkus@gmail.com>
2024-02-07 21:45:03 +0100

    Persist app state in cookie

--- crowdfunding-product-page/src/pages/pledge.ts
+++ crowdfunding-product-page/src/pages/pledge.ts
@@ -1,18 +1,31 @@
 import type { APIRoute } from "astro";
+import { COOKIE_NAME } from "../utils/cookie";
+import { z } from "zod";
 
-const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+const StateSchema = z.object({
+  isBookmarked: z.boolean().default(false),
+  hasPledged: z.boolean().default(false),
+  amount: z.number().default(0),
+});
 
-export const POST: APIRoute = async ({ request }) => {
-  await sleep(3000);
-  console.log(Object.fromEntries(await request.formData()));
-  /*
-    todo:
-    1. parse/validate
-    2. set cookie
-  */
-  return new Response(
-    JSON.stringify({
-      message: "Success",
-    })
-  );
+const PledgeInput = z.object({
+  id: z.string(),
+  amount: z.coerce.number().nonnegative(),
+  // todo: check id + min
+});
+
+export const POST: APIRoute = async ({ request, cookies }) => {
+  const formData = await request.formData();
+  const result = PledgeInput.safeParse(Object.fromEntries(formData));
+  if (!result.success) {
+    return new Response(null, { status: 400 });
+  }
+  const json = cookies.get(COOKIE_NAME)?.json();
+  const state = StateSchema.parse(json ?? {});
+  cookies.set(COOKIE_NAME, {
+    ...state,
+    hasPledged: true,
+    amount: state.amount + result.data.amount,
+  });
+  return new Response();
 };
