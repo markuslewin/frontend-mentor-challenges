8800f2f0eab8daf99d281cc4d729aff57c132844
Markus Lewin <lewinmarkus@gmail.com>
2024-02-13 15:23:44 +0100

    Move colors to theme

--- frontend-quiz-app/app/utils/extended-theme.ts
+++ frontend-quiz-app/app/utils/extended-theme.ts
@@ -2,47 +2,19 @@ import { type Config } from 'tailwindcss'
 
 export const extendedTheme = {
 	colors: {
-		border: 'hsl(var(--border))',
-		input: {
-			DEFAULT: 'hsl(var(--input))',
-			invalid: 'hsl(var(--input-invalid))',
-		},
-		ring: {
-			DEFAULT: 'hsl(var(--ring))',
-			invalid: 'hsl(var(--foreground-destructive))',
-		},
 		background: 'hsl(var(--background))',
 		foreground: {
 			DEFAULT: 'hsl(var(--foreground))',
-			destructive: 'hsl(var(--foreground-destructive))',
-		},
-		primary: {
-			DEFAULT: 'hsl(var(--primary))',
-			foreground: 'hsl(var(--primary-foreground))',
-		},
-		secondary: {
-			DEFAULT: 'hsl(var(--secondary))',
-			foreground: 'hsl(var(--secondary-foreground))',
-		},
-		destructive: {
-			DEFAULT: 'hsl(var(--destructive))',
-			foreground: 'hsl(var(--destructive-foreground))',
-		},
-		muted: {
-			DEFAULT: 'hsl(var(--muted))',
-			foreground: 'hsl(var(--muted-foreground))',
-		},
-		accent: {
-			DEFAULT: 'hsl(var(--accent))',
-			foreground: 'hsl(var(--accent-foreground))',
-		},
-		popover: {
-			DEFAULT: 'hsl(var(--popover))',
-			foreground: 'hsl(var(--popover-foreground))',
+			heading: 'hsl(var(--foreground-heading))',
 		},
 		card: {
 			DEFAULT: 'hsl(var(--card))',
 			foreground: 'hsl(var(--card-foreground))',
+			shadow: 'hsl(var(--card-shadow))',
+		},
+		button: {
+			DEFAULT: 'hsl(var(--button))',
+			foreground: 'hsl(var(--button-foreground))',
 		},
 	},
 	borderColor: {
--- frontend-quiz-app/tailwind.config.ts
+++ frontend-quiz-app/tailwind.config.ts
@@ -19,17 +19,6 @@ export default {
 				'2xl': '1400px',
 			},
 		},
-		colors: {
-			purple: 'hsl(277 91% 56%)',
-			'dark-navy': 'hsl(216 25% 25%)',
-			navy: 'hsl(215 27% 32%)',
-			'grey-navy': 'hsl(219 13% 44%)',
-			'light-bluish': 'hsl(216 47% 78%)',
-			'light-grey': 'hsl(220 38% 97%)',
-			'pure-white': 'hsl(0 0% 100%)',
-			green: 'hsl(151 70% 50%)',
-			red: 'hsl(0 82% 63%)',
-		},
 		fontFamily: {
 			body: '"Rubik Variable", sans-serif',
 		},
@@ -41,9 +30,10 @@ export default {
 			'body-m': rem(24),
 			'body-s': rem(20),
 		},
-		extend: {
-			...extendedTheme,
+		boxShadow: {
+			default: '0 1rem 2.5rem',
 		},
+		extend: extendedTheme,
 	},
 	presets: [marketingPreset],
 	plugins: [animatePlugin, radixPlugin],
