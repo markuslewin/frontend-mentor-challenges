a1ce1b20b637332551356f6a1e722e007c723c87
Markus Lewin <lewinmarkus@gmail.com>
2024-02-27 20:37:41 +0100

    Fetch data from api

--- dictionary-web-app/app/routes/$word.tsx
+++ dictionary-web-app/app/routes/$word.tsx
@@ -1,20 +1,87 @@
-import { invariant } from "@epic-web/invariant";
+import { invariantResponse } from "@epic-web/invariant";
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { z } from "zod";
 
-export function loader({ params }: LoaderFunctionArgs) {
+// We don't know a lot
+const DefinitionsSchema = z.array(
+  z
+    .object({
+      word: z.string(),
+      phonetic: z.string(),
+      phonetics: z.array(
+        z.object({ text: z.string(), audio: z.string() }).partial(),
+      ),
+      meanings: z.array(
+        z
+          .object({
+            partOfSpeech: z.string(),
+            definitions: z.array(
+              z
+                .object({
+                  definition: z.string(),
+                  synonyms: z.array(z.string()),
+                  antonyms: z.array(z.string()),
+                  example: z.string(),
+                })
+                .partial(),
+            ),
+            synonyms: z.array(z.string()),
+            antonyms: z.array(z.string()),
+          })
+          .partial(),
+      ),
+      sourceUrls: z.array(z.string()),
+    })
+    .partial(),
+);
+
+export async function loader({ params }: LoaderFunctionArgs) {
   const { word } = params;
-  invariant(word, "Invalid word");
-  return { word };
+  invariantResponse(word, "Invalid word");
+
+  const response = await fetch(
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${encodeURIComponent(word)}`,
+  );
+  if (response.status === 404) {
+    // "Not found" screen
+    return null;
+  }
+  invariantResponse(response.status !== 429, "Too many requests", {
+    status: 429,
+  });
+  invariantResponse(response.ok, "Invalid response", { status: 500 });
+
+  let json;
+  try {
+    json = await response.json();
+  } catch {
+    throw new Response("Failed to parse response", { status: 500 });
+  }
+
+  const result = DefinitionsSchema.safeParse(json);
+  invariantResponse(result.success, "Invalid shape of response", {
+    status: 500,
+  });
+  const definition = result.data[0];
+  if (!definition) {
+    // "Not found" screen
+    return null;
+  }
+
+  return { definition };
 }
 
 export default function Word() {
-  const { word } = useLoaderData<typeof loader>();
+  const data = useLoaderData<typeof loader>();
+
   return (
     <>
-      <h1>
-        todo: Fetch data for <code>{word}</code>
-      </h1>
+      {data ? (
+        <pre>{JSON.stringify(data.definition, undefined, "\t")}</pre>
+      ) : (
+        <p>Not found</p>
+      )}
     </>
   );
 }
