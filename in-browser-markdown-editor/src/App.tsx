5c8ded1613c2d06b76d9f3bc7507b7f91054f52f
Markus Lewin <lewinmarkus@gmail.com>
2024-03-13 19:05:22 +0100

    Add markup

--- in-browser-markdown-editor/src/App.tsx
+++ in-browser-markdown-editor/src/App.tsx
@@ -1,33 +1,121 @@
-import "./App.css";
+import documents from "./data/data.json";
 
 function App() {
   return (
     <>
-      {/*
-<!-- Sidebar start -->
-
-My Documents
-
-New Document
-
-<!-- Sidebar end -->
-
-<!-- Header start -->
-
-Document Name
-
-Save Changes
-
-<!-- Header end -->
-
-<!-- Main start -->
-
-Markdown
-
-Preview
-
-<!-- Main end -->
-*/}
+      <header>
+        <div>
+          <h1>
+            <img alt="Markdown" src="/assets/logo.svg" />
+          </h1>
+          <button>
+            <img alt="Open menu" src="/assets/icon-menu.svg" />
+            <img alt="Close menu" src="/assets/icon-close.svg" />
+          </button>
+          {/* https://www.radix-ui.com/primitives/docs/components/dialog */}
+          <div>
+            <h2>My documents</h2>
+            <button>
+              <span aria-hidden="true">+ </span>New document
+            </button>
+            <ul>
+              {documents
+                .map((document, i) => {
+                  return { ...document, id: i };
+                })
+                .map((document) => {
+                  // todo: Parse and format dates from `document.createdAt`
+                  const dateTime = "2022-01-04";
+                  const text = "01 April 2022";
+                  return (
+                    <li key={document.id}>
+                      <img alt="" src="/assets/icon-document.svg" />
+                      <a href="#">{document.name}</a>
+                      <p>
+                        <time dateTime={dateTime}>{text}</time>
+                      </p>
+                    </li>
+                  );
+                })}
+            </ul>
+            <h2>Switch mode</h2>
+            <form>
+              <button type="submit">
+                <img alt="" src="/assets/icon-dark-mode.svg" />
+                <span>Switch to dark mode</span>
+                <img alt="" src="/assets/icon-light-mode.svg" />
+              </button>
+              <p>
+                <output>
+                  <span>Light mode is now active</span>
+                </output>
+              </p>
+            </form>
+          </div>
+        </div>
+        <div>
+          <h2>Options</h2>
+          <label>
+            <img alt="" src="/assets/icon-document.svg" />
+            <div>
+              <span>Document name</span>
+              <input
+                name="document-name"
+                defaultValue="welcome-to-markdown.md"
+              />
+            </div>
+          </label>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <button>
+                <img alt="Delete document" src="/assets/icon-delete.svg" />
+              </button>
+              {/* https://www.radix-ui.com/primitives/docs/components/alert-dialog */}
+              <div>
+                <h2>Delete this document?</h2>
+                <p>
+                  Are you sure you want to delete the ‘welcome.md’ document and
+                  its contents? This action cannot be reversed.
+                </p>
+                <button>Confirm & delete</button>
+              </div>
+            </li>
+            <li>
+              <button>
+                <img alt="" src="/assets/icon-save.svg" />
+                <span>Save changes</span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </header>
+      <main>
+        <h2>Document</h2>
+        <div>
+          <h3>Markdown</h3>
+          <button>
+            <img alt="Show preview" src="/assets/icon-show-preview.svg" />
+          </button>
+        </div>
+        <label>
+          <span>Markdown</span>
+          <textarea name="markdown" defaultValue="# Welcome to Markdown" />
+        </label>
+        <div>
+          <h3>Preview</h3>
+          <button>
+            <img alt="Show preview" src="/assets/icon-show-preview.svg" />
+          </button>
+          <button>
+            <img alt="Hide preview" src="/assets/icon-hide-preview.svg" />
+          </button>
+        </div>
+        <div
+          dangerouslySetInnerHTML={{ __html: "<h4>Welcome to Markdown</h4>" }}
+        />
+      </main>
     </>
   );
 }
