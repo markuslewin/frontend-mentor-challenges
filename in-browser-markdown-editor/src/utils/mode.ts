272b54f9dcd6a3bedf32752dfd9ca157adf15da5
Markus Lewin <lewinmarkus@gmail.com>
2024-03-15 20:50:29 +0100

    Persist mode

--- in-browser-markdown-editor/src/utils/mode.ts
+++ in-browser-markdown-editor/src/utils/mode.ts
@@ -1,8 +1,23 @@
 import { useEffect, useRef, useState } from "react";
+import { z } from "zod";
+
+const modeKey = "mode";
+
+const ModeSchema = z.union([z.literal("light"), z.literal("dark")]);
+
+function getStoredMode() {
+  const rawMode = localStorage.getItem(modeKey);
+  const result = ModeSchema.safeParse(rawMode);
+  return result.success ? result.data : null;
+}
+
+function setStoredMode(mode: z.infer<typeof ModeSchema>) {
+  localStorage.setItem(modeKey, mode);
+}
 
 export function useMode() {
   const [selectedMode, setSelectedMode] = useState<"light" | "dark" | null>(
-    null
+    getStoredMode()
   );
   const darkSchemeMqlRef = useRef(matchMedia("(prefers-color-scheme: dark)"));
   const [prefersDarkScheme, setPrefersDarkScheme] = useState(
@@ -26,6 +41,7 @@ export function useMode() {
     mode: selectedMode ?? (prefersDarkScheme ? "dark" : "light"),
     selectMode(mode: "light" | "dark") {
       setSelectedMode(mode);
+      setStoredMode(mode);
     },
   };
 }
