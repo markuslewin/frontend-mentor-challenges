0492c80afe2e730c759a26bd916063d6b9d70670
Markus Lewin <lewinmarkus@gmail.com>
2024-04-07 20:34:45 +0200

    Add markup

--- interactive-comments-section/src/App.tsx
+++ interactive-comments-section/src/App.tsx
@@ -1,35 +1,138 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { currentUser, comments } from "./data/data.json";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Interactive comments section</h1>
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
+      <section aria-labelledby="add-comment-label">
+        <h2 id="add-comment-label">Add a comment</h2>
+        <form>
+          <label>
+            Add a comment{" "}
+            <textarea name="content" placeholder="Add a comment…"></textarea>
+          </label>
+          <p>
+            Commenting as{" "}
+            <Avatar alt={currentUser.username} image={currentUser.image} />
+          </p>
+          <button type="submit">Send</button>
+        </form>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+type Comment = (typeof comments)[number];
+
+function Comment({ comment }: { comment: Comment }) {
+  return (
+    <article>
+      <footer>
+        <Avatar alt="" image={comment.user.image} />
+        <p>{comment.user.username}</p>
+        <p>{comment.createdAt}</p>
+      </footer>
+      <p>{comment.content}</p>
+      <Score id={comment.id} score={comment.score} />
+      <Mutate id={comment.id} />
+      {comment.replies.map((reply) => (
+        <Reply key={reply.id} reply={reply} />
+      ))}
+    </article>
+  );
+}
+
+type Reply = Comment["replies"][number];
+
+function Reply({ reply }: { reply: Reply }) {
+  return (
+    <article>
+      <footer>
+        <Avatar alt="" image={reply.user.image} />
+        <p>{reply.user.username}</p>
+        <p>{reply.createdAt}</p>
+      </footer>
+      <p>
+        <b>@{reply.replyingTo}</b> {reply.content}
+      </p>
+      <Score id={reply.id} score={reply.score} />
+      <Mutate id={reply.id} />
+    </article>
+  );
+}
+
+function Avatar({
+  alt,
+  image,
+}: {
+  alt: string;
+  image: {
+    webp: string;
+    png: string;
+  };
+}) {
+  return (
+    <picture>
+      <source type="image/webp" srcSet={image.webp} />
+      <img alt={alt} width={64} height={64} src={image.png} />
+    </picture>
+  );
+}
+
+function Score({ id, score }: { id: number; score: number }) {
+  return (
+    <>
+      <p>{score} points</p>
+      <ul role="list">
+        <li>
+          <form>
+            <input type="hidden" name="intent" value="upvote-message" />
+            <input type="hidden" name="id" value={id} />
+            <button type="submit">
+              <img alt="Upvote" src="/images/icon-plus.svg" />
+            </button>
+          </form>
+        </li>
+        <li>
+          <form>
+            <input type="hidden" name="intent" value="downvote-message" />
+            <input type="hidden" name="id" value={id} />
+            <button type="submit">
+              <img alt="Downvote" src="/images/icon-minus.svg" />
+            </button>
+          </form>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+function Mutate({ id }: { id: number }) {
+  return (
+    <ul role="list">
+      <li>
+        <form>
+          <input type="hidden" name="intent" value="delete-message" />
+          <input type="hidden" name="id" value={id} />
+          <button type="submit">
+            <img alt="" src="/images/icon-delete.svg" /> Delete
+          </button>
+        </form>
+      </li>
+      <li>
+        <form>
+          <input type="hidden" name="intent" value="edit-message" />
+          <input type="hidden" name="id" value={id} />
+          <button type="submit">
+            <img alt="" src="/images/icon-edit.svg" /> Edit
+          </button>
+        </form>
+      </li>
+    </ul>
+  );
+}
+
+export default App;
