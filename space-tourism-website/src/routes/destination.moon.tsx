5a117cffb8c5a6317f3ff3986879b0a550ed5d06
Markus Lewin <lewinmarkus@gmail.com>
2024-04-23 15:21:11 +0200

    Style moon

--- space-tourism-website/src/routes/destination.moon.tsx
+++ space-tourism-website/src/routes/destination.moon.tsx
@@ -1,77 +1,114 @@
-import { NavLink } from "react-router-dom";
+import { ReactNode, useId } from "react";
+import { NavLink, NavLinkProps } from "react-router-dom";
 
 export function DestinationMoonRoute() {
+  const destinationsHeadingId = useId();
+
   return (
-    <div className="mt-8 tablet:mt-[3.75rem] desktop:mt-16 desktop:grid desktop:grid-cols-2">
-      <picture>
+    <div className="mt-8 tablet:mt-[3.75rem] desktop:mt-16 pb-14 tablet:pb-16 desktop:pb-28 desktop:grid desktop:grid-cols-[minmax(0,31.8125rem)_minmax(0,27.8125rem)] desktop:justify-between desktop:gap-12">
+      <picture className="place-self-end">
         <source
           type="image/webp"
           srcSet="/assets/destination/image-moon.webp"
         />
         <img
-          className="desktop:ml-16 w-[10.625rem] tablet:w-[18.75rem] desktop:w-[27.8125rem]"
+          className="mx-auto desktop:mx-0 max-w-[10.625rem] tablet:max-w-[18.75rem] desktop:max-w-full"
           alt=""
           src="/assets/destination/image-moon.png"
         />
       </picture>
-      <div>
-        <h2>Moon</h2>
-        {/* todo: Name nav */}
-        <nav>
-          <ul className="flex flex-wrap gap-4">
-            <li>
-              <NavLink to="/destination">Moon</NavLink>
-            </li>
-            <li>
-              <NavLink to="/destination/mars">Mars</NavLink>
-            </li>
-            <li>
-              <NavLink to="/destination/europa">Europa</NavLink>
-            </li>
-            <li>
-              <NavLink to="/destination/titan">Titan</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <article className="mt-7 tablet:mt-14 desktop:mt-0 mx-auto desktop:mx-0 max-w-[35.8125rem] desktop:max-w-[27.8125rem]">
+        <header>
+          <nav
+            className="font-barlow-condensed text-nav-text uppercase"
+            aria-labelledby={destinationsHeadingId}
+          >
+            <h2 className="sr-only" id={destinationsHeadingId}>
+              Destinations
+            </h2>
+            <ul className="mx-auto desktop:mx-0 max-w-[14.875rem] tablet:max-w-[17.875rem] flex justify-between">
+              <li>
+                <DestinationNavLink to="/destination">Moon</DestinationNavLink>
+              </li>
+              <li>
+                <DestinationNavLink to="/destination/mars">
+                  Mars
+                </DestinationNavLink>
+              </li>
+              <li>
+                <DestinationNavLink to="/destination/europa">
+                  Europa
+                </DestinationNavLink>
+              </li>
+              <li>
+                <DestinationNavLink to="/destination/titan">
+                  Titan
+                </DestinationNavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <div className="text-center desktop:text-start">
+          <h2 className="text-FFFFFF mt-5 tablet:mt-8 desktop:mt-9 font-bellefair text-heading-3 tablet:text-[5rem] tablet:leading-[5.75rem] desktop:text-heading-2 uppercase">
+            Moon
+          </h2>
+          <p className="tablet:mt-2 desktop:mt-4">
+            See our planet as you’ve never seen it before. A perfect relaxing
+            trip away to help regain perspective and come back refreshed. While
+            you’re there, take in some history by visiting the Luna 2 and Apollo
+            11 landing sites.
+          </p>
+        </div>
+        <hr className="text-[hsl(231,15%,26%)] mt-8 tablet:mt-12 desktop:mt-14" />
+        <div className="mt-8 tablet:mt-7 tablet:mx-auto desktop:mx-0 tablet:max-w-[22.5rem] desktop:max-w-none grid tablet:grid-cols-[auto_auto] tablet:justify-between desktop:grid-cols-2 gap-8 tablet:gap-0 text-center desktop:text-start">
+          <Stat>
+            <StatKey>Avg. distance</StatKey>
+            <StatValue>384,400 km</StatValue>
+          </Stat>
+          <Stat>
+            <StatKey>Est. travel time</StatKey>
+            <StatValue>3 days</StatValue>
+          </Stat>
+        </div>
+      </article>
     </div>
   );
 }
 
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="UTF-8">
-//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+interface DestinationNavLinkProps extends NavLinkProps {}
 
-//   <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png">
+function DestinationNavLink(props: DestinationNavLinkProps) {
+  return (
+    <NavLink
+      className={({ isActive }) =>
+        `block ${isActive ? "text-FFFFFF border-b-[3px] pb-3" : ""}`
+      }
+      {...props}
+    />
+  );
+}
 
-//   <title>Frontend Mentor | Space tourism website</title>
-// </head>
-// <body>
+function Stat({ children }: { children: ReactNode }) {
+  return (
+    <p className="font-barlow-condensed text-subheading-2 uppercase">
+      {children}
+    </p>
+  );
+}
 
-//   00 Home
-//   01 Destination
-//   02 Crew
-//   03 Technology
+function StatKey({ children }: { children: ReactNode }) {
+  return (
+    <>
+      {children}
+      <span className="sr-only">: </span>
+    </>
+  );
+}
 
-//   01 Pick your destination
-
-//   Moon
-//   Mars
-//   Europa
-//   Titan
-
-//   Moon
-
-//   See our planet as you’ve never seen it before. A perfect relaxing trip away to help
-//   regain perspective and come back refreshed. While you’re there, take in some history
-//   by visiting the Luna 2 and Apollo 11 landing sites.
-
-//   Avg. distance
-//   384,400 km
-
-//   Est. travel time
-//   3 days
-// </body>
-// </html>
+function StatValue({ children }: { children: ReactNode }) {
+  return (
+    <strong className="text-FFFFFF mt-3 block font-bellefair text-subheading-1 tracking-normal">
+      {children}
+    </strong>
+  );
+}
