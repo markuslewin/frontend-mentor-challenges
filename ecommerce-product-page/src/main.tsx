5c6eee2847063a0dfab43e8fe26bae46ba0f4f8a
Markus Lewin <lewinmarkus@gmail.com>
2024-04-30 15:51:53 +0200

    Add design values

--- ecommerce-product-page/src/main.tsx
+++ ecommerce-product-page/src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+// Supports weights 100-900
+import "@fontsource-variable/kumbh-sans";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- ecommerce-product-page/tailwind.config.js
+++ ecommerce-product-page/tailwind.config.js
@@ -9,19 +9,24 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens,
-    // todo: Add colors
-    // colors: {
-    //   "moderate-blue": "hsl(238 40% 52%)",
-    // },
+    colors: {
+      // Primary
+      orange: "hsl(26 100% 55%)",
+      "pale-orange": "hsl(25 100% 94%)",
+      // Neutral
+      "very-dark-blue": "hsl(220 13% 13%)",
+      "dark-grayish-blue": "hsl(219 9% 45%)",
+      "grayish-blue": "hsl(220 14% 75%)",
+      "light-grayish-blue": "hsl(223 64% 98%)",
+      white: "hsl(0 0% 100%)",
+      black: "hsl(0 0% 0%)",
+    },
     fontFamily: {
-      // todo: Add font from Fontsource
-      base: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
+      "kumbh-sans": "'Kumbh Sans Variable', sans-serif",
     },
     fontSize: {
-      // todo: Add font sizes
-      "heading-l": [rem(48), { fontWeight: 500, lineHeight: rem(48) }],
-      "heading-m": [rem(39), { fontWeight: 500, lineHeight: rem(39) }],
-      body: [rem(20), { lineHeight: rem(24) }],
+      body: [rem(15), { lineHeight: rem(25) }],
+      "body-tablet": [rem(16), { lineHeight: rem(26) }],
     },
     extend: {},
   },
