16b7b84c5dfdd2de1071321ad0be3cb9c8e7bcfd
Markus Lewin <lewinmarkus@gmail.com>
2024-05-06 21:16:27 +0200

    Style mobile painting route

--- galleria-slideshow-site/src/routes/$paintingName.tsx
+++ galleria-slideshow-site/src/routes/$paintingName.tsx
@@ -3,6 +3,7 @@ import { invariantResponse } from "@epic-web/invariant";
 import { paintings } from "../utils/paintings";
 import { screens } from "../utils/screens";
 import { Icon } from "../components/icon";
+import styles from "./$paintingName.module.css";
 
 export function loader({ params }: LoaderFunctionArgs) {
   const { paintingName } = params;
@@ -25,77 +26,91 @@ export function PaintingRoute() {
   const { painting } = useLoaderData() as ReturnType<typeof loader>;
 
   return (
-    <article>
-      <header>
+    <div className={`[ ${styles.route} ] [ center ]`}>
+      <article className={styles.layout}>
+        {/* <header>
         <progress />
         <div>
-          <p>
-            <span>Painting: </span>
-            {painting.name}
+        <p>
+        <span>Painting: </span>
+        {painting.name}
+        </p>
+        <p>
+        <span>Artist: </span>
+        {painting.artist.name}
           </p>
-          <p>
-            <span>Artist: </span>
+          </div>
+          <nav>
+          <ul role="list">
+            <li>
+            <Link to="#" aria-disabled="true">
+            <Icon name="icon-back-button" />
+            <span>Previous painting</span>
+            </Link>
+            </li>
+            <li>
+            <Link to="/Girl with a Pearl Earring">
+            <Icon name="icon-next-button" />
+            <span>Next painting</span>
+            </Link>
+            </li>
+            </ul>
+            </nav>
+          </header> */}
+        <div className={styles.title}>
+          <h1 className={styles["title__name"]}>{painting.name}</h1>
+          <p className={styles["title__artist"]}>
+            <span className="sr-only">By: </span>
             {painting.artist.name}
           </p>
         </div>
-        <nav>
-          <ul role="list">
-            <li>
-              <Link to="#" aria-disabled="true">
-                <Icon name="icon-back-button" />
-                <span>Previous painting</span>
+        <img
+          className={styles.artist}
+          alt=""
+          width={painting.artist.image.width}
+          height={painting.artist.image.height}
+          src={painting.artist.image.src}
+        />
+        <div className={styles.hero}>
+          <picture className={styles["hero__image"]}>
+            <source
+              media={`(min-width: ${screens.tablet})`}
+              width={painting.images.hero.large.width}
+              height={painting.images.hero.large.height}
+              srcSet={painting.images.hero.large.src}
+            />
+            <img
+              alt={`todo: Visual description of "${painting.name}"`}
+              width={painting.images.hero.small.width}
+              height={painting.images.hero.small.height}
+              src={painting.images.hero.small.src}
+            />
+          </picture>
+          <p className={styles["hero__button-container"]}>
+            <button className={styles["hero__button"]} type="button">
+              <Icon
+                className={styles["hero__button-icon"]}
+                name="icon-view-image"
+              />
+              <span className={styles["hero__button-text"]}>View image</span>
+            </button>
+          </p>
+        </div>
+        <div className={styles["description-container"]}>
+          <p className={styles["description__year"]}>{painting.year}</p>
+          <div className={styles["description__text"]}>
+            <p>{painting.description}</p>
+            <p className={styles["description__source-container"]}>
+              <Link
+                className={styles["description__source"]}
+                to={painting.source}
+              >
+                Go to source
               </Link>
-            </li>
-            <li>
-              <Link to="/Girl with a Pearl Earring">
-                <Icon name="icon-next-button" />
-                <span>Next painting</span>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <div>
-        <h1>{painting.name}</h1>
-        <p>
-          <span>By: </span>
-          {painting.artist.name}
-        </p>
-      </div>
-      <img
-        alt=""
-        width={painting.artist.image.width}
-        height={painting.artist.image.height}
-        src={painting.artist.image.src}
-      />
-      <div>
-        <picture>
-          <source
-            media={`(min-width: ${screens.tablet})`}
-            width={painting.images.hero.large.width}
-            height={painting.images.hero.large.height}
-            srcSet={painting.images.hero.large.src}
-          />
-          <img
-            alt={`todo: Visual description of "${painting.name}"`}
-            width={painting.images.hero.small.width}
-            height={painting.images.hero.small.height}
-            src={painting.images.hero.small.src}
-          />
-        </picture>
-        <p>
-          <button type="button">
-            <Icon name="icon-view-image" /> View image
-          </button>
-        </p>
-      </div>
-      <div>
-        <p>{painting.year}</p>
-        <p>{painting.description}</p>
-        <p>
-          <Link to={painting.source}>Go to source</Link>
-        </p>
-      </div>
-    </article>
+            </p>
+          </div>
+        </div>
+      </article>
+    </div>
   );
 }
