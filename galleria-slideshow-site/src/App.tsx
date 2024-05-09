0458c7561feed7117095b9836e348dc82da48996
Markus Lewin <lewinmarkus@gmail.com>
2024-05-09 13:18:48 +0200

    Remove template files

--- galleria-slideshow-site/src/App.tsx
+++ galleria-slideshow-site/src/App.tsx
@@ -1,19 +1,11 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Layout } from "./components/layout";
 import { Home, handle as homeHandle } from "./routes/home";
-import { ApiEndpoint } from "./routes/api-endpoint";
-import { FormValidation } from "./routes/form-validation";
-import { NestedRoutes } from "./routes/nested-routes";
-import {
-  NestedRoutesCreate,
-  action as nestedRoutesCreateAction,
-} from "./routes/nested-routes.create";
 import {
   PaintingRoute,
   handle as paintingHandle,
   loader as paintingLoader,
 } from "./routes/$paintingName";
-import { OptimizedImage } from "./routes/optimized-image";
 
 const router = createBrowserRouter([
   {
@@ -30,29 +22,6 @@ const router = createBrowserRouter([
         handle: paintingHandle,
         loader: paintingLoader,
         Component: PaintingRoute,
-      },
-      {
-        path: "api-endpoint",
-        Component: ApiEndpoint,
-      },
-      {
-        path: "form-validation",
-        Component: FormValidation,
-      },
-      {
-        path: "nested-routes",
-        Component: NestedRoutes,
-        children: [
-          {
-            path: "create",
-            action: nestedRoutesCreateAction,
-            Component: NestedRoutesCreate,
-          },
-        ],
-      },
-      {
-        path: "optimized-image",
-        Component: OptimizedImage,
       },
     ],
   },
--- galleria-slideshow-site/src/utils/seed.ts
+++
@@ -1,9 +0,0 @@
-import { createMessage, getMessages } from "./messages";
-
-(function () {
-  if (getMessages()) return;
-
-  for (const text of ["One", "Two", "Three"]) {
-    createMessage({ text });
-  }
-})();
