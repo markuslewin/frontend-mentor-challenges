11eb284ae844d418128e83ed297db164da5a5313
Markus Lewin <lewinmarkus@gmail.com>
2024-05-12 21:19:42 +0200

    Add mobile menu

--- planets-fact-site/src/components/layout.tsx
+++ planets-fact-site/src/components/layout.tsx
@@ -6,6 +6,7 @@ import { screens } from "../utils/screens";
 import styles from "./layout.module.css";
 import { Icon } from "./icon";
 import { colorByPlanet } from "../utils/planets/planets";
+import { ReactNode, useState } from "react";
 
 export function Layout() {
   const tabletMatches = useMedia(`(min-width: ${screens.tablet})`);
@@ -19,125 +20,48 @@ export function Layout() {
             {tabletMatches ? (
               <ul className={`[ ${styles["header__links"]} ] [ cluster ]`}>
                 <li>
-                  <NavLink
-                    className={styles["header__link"]}
-                    style={{
-                      ["--header-link-color" as string]: colorByPlanet.Mercury,
-                    }}
-                    to="/mercury"
-                    preventScrollReset
-                  >
+                  <PlanetLink to="/mercury" color={colorByPlanet.Mercury}>
                     Mercury
-                  </NavLink>
+                  </PlanetLink>
                 </li>
                 <li>
-                  <NavLink
-                    className={styles["header__link"]}
-                    style={{
-                      ["--header-link-color" as string]: colorByPlanet.Venus,
-                    }}
-                    to="/venus"
-                    preventScrollReset
-                  >
+                  <PlanetLink to="/venus" color={colorByPlanet.Venus}>
                     Venus
-                  </NavLink>
+                  </PlanetLink>
                 </li>
                 <li>
-                  <NavLink
-                    className={styles["header__link"]}
-                    style={{
-                      ["--header-link-color" as string]: colorByPlanet.Earth,
-                    }}
-                    to="/earth"
-                    preventScrollReset
-                  >
+                  <PlanetLink to="/earth" color={colorByPlanet.Earth}>
                     Earth
-                  </NavLink>
+                  </PlanetLink>
                 </li>
                 <li>
-                  <NavLink
-                    className={styles["header__link"]}
-                    style={{
-                      ["--header-link-color" as string]: colorByPlanet.Mars,
-                    }}
-                    to="/mars"
-                    preventScrollReset
-                  >
+                  <PlanetLink to="/mars" color={colorByPlanet.Mars}>
                     Mars
-                  </NavLink>
+                  </PlanetLink>
                 </li>
                 <li>
-                  <NavLink
-                    className={styles["header__link"]}
-                    style={{
-                      ["--header-link-color" as string]: colorByPlanet.Jupiter,
-                    }}
-                    to="/jupiter"
-                    preventScrollReset
-                  >
+                  <PlanetLink to="/jupiter" color={colorByPlanet.Jupiter}>
                     Jupiter
-                  </NavLink>
+                  </PlanetLink>
                 </li>
                 <li>
-                  <NavLink
-                    className={styles["header__link"]}
-                    style={{
-                      ["--header-link-color" as string]: colorByPlanet.Saturn,
-                    }}
-                    to="/saturn"
-                    preventScrollReset
-                  >
+                  <PlanetLink to="/saturn" color={colorByPlanet.Saturn}>
                     Saturn
-                  </NavLink>
+                  </PlanetLink>
                 </li>
                 <li>
-                  <NavLink
-                    className={styles["header__link"]}
-                    style={{
-                      ["--header-link-color" as string]: colorByPlanet.Uranus,
-                    }}
-                    to="/uranus"
-                    preventScrollReset
-                  >
+                  <PlanetLink to="/uranus" color={colorByPlanet.Uranus}>
                     Uranus
-                  </NavLink>
+                  </PlanetLink>
                 </li>
                 <li>
-                  <NavLink
-                    className={styles["header__link"]}
-                    style={{
-                      ["--header-link-color" as string]: colorByPlanet.Neptune,
-                    }}
-                    to="/neptune"
-                    preventScrollReset
-                  >
+                  <PlanetLink to="/neptune" color={colorByPlanet.Neptune}>
                     Neptune
-                  </NavLink>
+                  </PlanetLink>
                 </li>
               </ul>
             ) : (
-              <Dialog.Root>
-                <Dialog.Trigger className={styles["header__trigger"]}>
-                  <Icon
-                    className="w-6 h-auto"
-                    name="icon-hamburger"
-                    width="24"
-                    height="17"
-                  />
-                  <span className="sr-only">Open menu</span>
-                </Dialog.Trigger>
-                <Dialog.Portal>
-                  <Dialog.Overlay className="menu__overlay" />
-                  <Dialog.Content
-                    className="menu__content"
-                    aria-describedby={undefined}
-                  >
-                    <Dialog.Title className="sr-only">Menu</Dialog.Title>
-                    {/* <Dialog.Description /> */}
-                    <Dialog.Close>Close menu</Dialog.Close>
-                  </Dialog.Content>
-                </Dialog.Portal>
-              </Dialog.Root>
+              <Menu />
             )}
           </nav>
         </header>
@@ -149,4 +73,141 @@ export function Layout() {
       <RouteAnnouncer />
     </>
   );
+}
+
+function Menu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <Dialog.Root open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+      <Dialog.Trigger className={styles["header__trigger"]}>
+        <Icon
+          className="w-6 h-auto"
+          name="icon-hamburger"
+          width="24"
+          height="17"
+        />
+        <span className="sr-only">Open menu</span>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="menu__overlay" />
+        <Dialog.Content className="menu__content" aria-describedby={undefined}>
+          <Dialog.Title className="sr-only">Menu</Dialog.Title>
+          <Dialog.Close className="sr-only">Close menu</Dialog.Close>
+          <ul className={`${styles["header__links"]}`}>
+            <li>
+              <PlanetLink
+                to="/mercury"
+                color={colorByPlanet.Mercury}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                <span className={styles["header__link-planet"]} /> Mercury{" "}
+                <PlanetLinkIcon />
+              </PlanetLink>
+            </li>
+            <li>
+              <PlanetLink
+                to="/venus"
+                color={colorByPlanet.Venus}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                <span className={styles["header__link-planet"]} /> Venus{" "}
+                <PlanetLinkIcon />
+              </PlanetLink>
+            </li>
+            <li>
+              <PlanetLink
+                to="/earth"
+                color={colorByPlanet.Earth}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                <span className={styles["header__link-planet"]} /> Earth{" "}
+                <PlanetLinkIcon />
+              </PlanetLink>
+            </li>
+            <li>
+              <PlanetLink
+                to="/mars"
+                color={colorByPlanet.Mars}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                <span className={styles["header__link-planet"]} /> Mars{" "}
+                <PlanetLinkIcon />
+              </PlanetLink>
+            </li>
+            <li>
+              <PlanetLink
+                to="/jupiter"
+                color={colorByPlanet.Jupiter}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                <span className={styles["header__link-planet"]} /> Jupiter{" "}
+                <PlanetLinkIcon />
+              </PlanetLink>
+            </li>
+            <li>
+              <PlanetLink
+                to="/saturn"
+                color={colorByPlanet.Saturn}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                <span className={styles["header__link-planet"]} /> Saturn{" "}
+                <PlanetLinkIcon />
+              </PlanetLink>
+            </li>
+            <li>
+              <PlanetLink
+                to="/uranus"
+                color={colorByPlanet.Uranus}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                <span className={styles["header__link-planet"]} /> Uranus{" "}
+                <PlanetLinkIcon />
+              </PlanetLink>
+            </li>
+            <li>
+              <PlanetLink
+                to="/neptune"
+                color={colorByPlanet.Neptune}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                <span className={styles["header__link-planet"]} /> Neptune{" "}
+                <PlanetLinkIcon />
+              </PlanetLink>
+            </li>
+          </ul>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
+
+function PlanetLink({
+  color,
+  to,
+  children,
+  onClick,
+}: {
+  color: string;
+  to: string;
+  children: ReactNode;
+  onClick?(): void;
+}) {
+  return (
+    <NavLink
+      className={styles["header__link"]}
+      style={{
+        ["--header-link-color" as string]: color,
+      }}
+      to={to}
+      onClick={onClick}
+      preventScrollReset
+    >
+      {children}
+    </NavLink>
+  );
+}
+
+function PlanetLinkIcon() {
+  return <Icon className={styles["header__link-icon"]} name="icon-chevron" />;
 }
