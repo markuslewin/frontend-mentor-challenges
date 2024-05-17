26f8b47f3d87d8c9762d4aaafde26ca242e5071f
Markus Lewin <lewinmarkus@gmail.com>
2024-05-17 12:23:41 +0200

    Add starter files

--- coffeeroasters-subscription-site/src/root.tsx
+++ coffeeroasters-subscription-site/src/root.tsx
@@ -1,36 +1,8 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Layout } from "./components/layout";
-import { Home, handle as homeHandle } from "./routes/home";
-import {
-  ApiEndpoint,
-  handle as apiEndpointHandler,
-} from "./routes/api-endpoint";
-import {
-  FormValidation,
-  handle as formValidationHandle,
-} from "./routes/form-validation";
-import { NestedRoutes } from "./routes/nested-routes";
-import {
-  NestedRoutesIndex,
-  handle as nestedRoutesIndexHandle,
-  action as nestedRoutesIndexAction,
-  loader as nestedRoutesIndexLoader,
-} from "./routes/nested-routes.index";
-import {
-  NestedRoutesCreate,
-  handle as nestedRoutesCreateHandle,
-  action as nestedRoutesCreateAction,
-} from "./routes/nested-routes.create";
-import {
-  NestedRoutesUpdate,
-  loader as nestedRoutesUpdateLoader,
-  action as nestedRoutesUpdateAction,
-  handle as nestedRoutesUpdateHandle,
-} from "./routes/nested-routes.update.$id";
-import {
-  OptimizedImage,
-  handle as optimizedImageHandle,
-} from "./routes/optimized-image";
+import { HomeRoute, handle as homeHandle } from "./routes/home";
+import { AboutRoute, handle as aboutHandle } from "./routes/about";
+import { PlanRoute, handle as planHandle } from "./routes/plan";
 
 const router = createBrowserRouter([
   {
@@ -40,48 +12,17 @@ const router = createBrowserRouter([
       {
         index: true,
         handle: homeHandle,
-        Component: Home,
+        Component: HomeRoute,
       },
       {
-        path: "api-endpoint",
-        handle: apiEndpointHandler,
-        Component: ApiEndpoint,
+        path: "about",
+        handle: aboutHandle,
+        Component: AboutRoute,
       },
       {
-        path: "form-validation",
-        handle: formValidationHandle,
-        Component: FormValidation,
-      },
-      {
-        path: "nested-routes",
-        Component: NestedRoutes,
-        children: [
-          {
-            index: true,
-            handle: nestedRoutesIndexHandle,
-            loader: nestedRoutesIndexLoader,
-            action: nestedRoutesIndexAction,
-            Component: NestedRoutesIndex,
-          },
-          {
-            path: "create",
-            handle: nestedRoutesCreateHandle,
-            action: nestedRoutesCreateAction,
-            Component: NestedRoutesCreate,
-          },
-          {
-            path: "update/:id",
-            handle: nestedRoutesUpdateHandle,
-            loader: nestedRoutesUpdateLoader,
-            action: nestedRoutesUpdateAction,
-            Component: NestedRoutesUpdate,
-          },
-        ],
-      },
-      {
-        path: "optimized-image",
-        handle: optimizedImageHandle,
-        Component: OptimizedImage,
+        path: "plan",
+        handle: planHandle,
+        Component: PlanRoute,
       },
     ],
   },
