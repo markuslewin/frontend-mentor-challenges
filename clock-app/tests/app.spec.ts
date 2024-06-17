9d8676878866c2a3e77fc052392896fadc2a5985
Markus Lewin <lewinmarkus@gmail.com>
2024-06-17 14:48:39 +0200

    Add tests

---
+++ clock-app/tests/app.spec.ts
@@ -0,0 +1,73 @@
+import { test, expect, Page } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+});
+
+test.skip("displays quotes", async ({ page }) => {
+  const quote = page.getByRole("region", { name: "quote" });
+  const blockquote = quote.getByRole("blockquote");
+
+  await expect(blockquote).toHaveText(
+    "The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.",
+  );
+  await expect(blockquote).toHaveText("Ada Lovelace");
+
+  await page.getByRole("button", { name: "get a new quote" }).click();
+
+  await expect(blockquote).toHaveText("");
+  await expect(blockquote).toHaveText("");
+});
+
+test.skip("displays time", async ({ page }) => {
+  const time = getTimeRegion(page);
+
+  await expect(time).toHaveText("11:37");
+});
+
+test.skip("good morning in the morning", async ({ page }) => {
+  const time = getTimeRegion(page);
+
+  await expect(time).toHaveText("good morning");
+});
+
+test.skip("good afternoon in the afternoon", async ({ page }) => {
+  const time = getTimeRegion(page);
+
+  await expect(time).toHaveText("good afternoon");
+});
+
+test.skip("good evening in the evening", async ({ page }) => {
+  const time = getTimeRegion(page);
+
+  await expect(time).toHaveText("good evening");
+});
+
+test.skip("displays additional date and time information", async ({ page }) => {
+  const time = getTimeRegion(page);
+  const additional = page.getByRole("region", {
+    name: "additional information",
+  });
+  const moreButton = time.getByRole("button", { name: "more" });
+  const lessButton = time.getByRole("button", { name: "less" });
+
+  await expect(additional).not.toBeAttached();
+
+  await moreButton.click();
+
+  await expect(additional).toBeAttached();
+  await expect(moreButton).not.toBeAttached();
+  await expect(lessButton).toBeAttached();
+  await expect(additional).toHaveText("europe/london");
+  await expect(additional).toHaveText("295");
+  await expect(additional).toHaveText("5");
+  await expect(additional).toHaveText("42");
+
+  await lessButton.click();
+
+  await expect(additional).not.toBeAttached();
+});
+
+function getTimeRegion(page: Page) {
+  return page.getByRole("region", { name: "time" });
+}
