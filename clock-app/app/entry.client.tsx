1208069b6029e3c218b54482cbb3a5c93ee0720d
Markus Lewin <lewinmarkus@gmail.com>
2024-06-26 20:53:39 +0200

    Remove top-level await

--- clock-app/app/entry.client.tsx
+++ clock-app/app/entry.client.tsx
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "#app/root";
 import "#app/index.css";
 
-if (import.meta.env.VITE_MOCKS === "true") {
-  await import("#app/mocks");
+async function enableMocking() {
+  if (import.meta.env.VITE_MOCKS === "true") {
+    const { worker } = await import("#app/mocks");
+
+    return worker.start();
+  }
 }
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
--- clock-app/app/mocks/index.ts
+++ clock-app/app/mocks/index.ts
@@ -2,5 +2,3 @@ import { setupWorker } from "msw/browser";
 import { handlers as quotableHandlers } from "./quotable";
 
 export const worker = setupWorker(...quotableHandlers);
-
-await worker.start();
