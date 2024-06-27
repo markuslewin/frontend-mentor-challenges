3c3cd4ac02a5f237ffea16da849a6511b747945a
Markus Lewin <lewinmarkus@gmail.com>
2024-06-27 17:57:39 +0200

    Don't intercept asset requests

--- clock-app/app/mocks/index.ts
+++ clock-app/app/mocks/index.ts
@@ -1,4 +1,9 @@
 import { setupWorker } from "msw/browser";
 import { handlers as quotableHandlers } from "./quotable";
+import { http, passthrough } from "msw";
 
-export const worker = setupWorker(...quotableHandlers);
+export const worker = setupWorker(
+  ...quotableHandlers,
+  http.get(/http:\/\/localhost:\d+\/.*/, async () => passthrough()),
+  http.post(/http:\/\/localhost:\d+\/.*/, async () => passthrough()),
+);
