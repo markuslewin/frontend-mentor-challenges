a7d9e137aaadaf01ccaa36071aa12179424b4dd7
Markus Lewin <lewinmarkus@gmail.com>
2024-07-03 15:09:08 +0200

    Remove template files

--- clock-app/app/root.tsx
+++ clock-app/app/root.tsx
@@ -1,30 +1,17 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Layout } from "#app/components/layout";
 import { Home } from "#app/routes/home";
 import { AnnouncementProvider } from "#app/components/announcer/announcement-provider";
+import { Announcer } from "#app/components/announcer/announcer";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Layout,
-    children: [
-      {
-        index: true,
-        Component: Home,
-      },
-    ],
-  },
-]);
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AnnouncementProvider>
-        <RouterProvider router={router} />
+        <Home />
+        <Announcer />
       </AnnouncementProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
