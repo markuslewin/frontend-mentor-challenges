fbd5a3f91b8b98d49faf3c4b5e7a9d620439a4b7
Markus Lewin <lewinmarkus@gmail.com>
2024-07-18 18:36:50 +0200

    Remove template files

--- static-job-listings/app/routes/home/routing.ts
+++
@@ -1,7 +0,0 @@
-import { type AnnouncementHandle } from '#app/components/route-announcer'
-
-export const handle = {
-	announcement() {
-		return 'Home'
-	},
-} satisfies AnnouncementHandle
--- static-job-listings/tests/mocks/index.ts
+++
@@ -1,9 +0,0 @@
-import { http, passthrough } from 'msw'
-import { setupWorker } from 'msw/browser'
-import { handlers as worldtimeapiHandlers } from '#tests/mocks/worldtimeapi'
-
-export const worker = setupWorker(
-	...worldtimeapiHandlers,
-	http.get(/http:\/\/localhost:\d+\/.*/, async () => passthrough()),
-	http.post(/http:\/\/localhost:\d+\/.*/, async () => passthrough()),
-)
