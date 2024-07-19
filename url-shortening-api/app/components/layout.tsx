1758fa59f4aaeb55c63f3a0a6d146f65fdcdb80e
Markus Lewin <lewinmarkus@gmail.com>
2024-07-19 19:54:24 +0200

    Add markup for header

--- url-shortening-api/app/components/layout.tsx
+++ url-shortening-api/app/components/layout.tsx
@@ -1,88 +1,56 @@
-import * as Dialog from '@radix-ui/react-dialog'
 import { useMediaQuery } from '@uidotdev/usehooks'
-import { cva } from 'class-variance-authority'
-import {
-	NavLink,
-	type NavLinkProps,
-	Outlet,
-	ScrollRestoration,
-} from 'react-router-dom'
+import { Form, Link, Outlet, ScrollRestoration } from 'react-router-dom'
 import { Announcer } from '#app/components/announcer'
+import { Icon } from '#app/components/icon'
 import { RouteAnnouncer } from '#app/components/route-announcer'
 import { media } from '#app/utils/screens'
-import { useTheme } from '#app/utils/theme'
-
-// todo
-//   Features
-//   Pricing
-//   Resources
-
-//   Login
-//   Sign Up
 
 export function Layout() {
-	const { theme, setTheme } = useTheme()
 	const tabletMatches = useMediaQuery(media.tablet)
-
-	const nextTheme = theme === 'dark' ? 'light' : 'dark'
 
 	return (
 		<>
 			<div className="min-h-screen center-gutter-4 tablet:center-gutter-10">
 				<header className="center-5xl">
-					<div className="flex flex-wrap justify-between gap-6 py-6">
-						<p>Logo</p>
-						<div className="flex flex-wrap items-center gap-4">
-							<button type="button" onClick={() => setTheme(nextTheme)}>
-								Switch to {nextTheme} mode
-							</button>
-							<p className="sr-only" aria-live="assertive">
-								{theme} mode is enabled.
-							</p>
-							<nav>
-								{tabletMatches ? (
-									<ul className="flex flex-wrap gap-4">
+					<div>
+						<p>
+							<Link to="/">
+								<Icon name="logo" />
+								<span>Shortly</span>
+							</Link>
+						</p>
+						<nav>
+							{tabletMatches ? (
+								<>
+									<ul role="list">
+										<DesktopLink name="Features" />
+										<DesktopLink name="Pricing" />
+										<DesktopLink name="Resources" />
+									</ul>
+									<ul role="list">
 										<li>
-											<MyNavLink to="/">Home</MyNavLink>
+											<Link to="#">Login</Link>
 										</li>
 										<li>
-											<MyNavLink to="/nested-routes">Nested routes</MyNavLink>
+											<Link to="#">Sign Up</Link>
 										</li>
 									</ul>
-								) : (
-									<Dialog.Root>
-										<Dialog.Trigger>Open menu</Dialog.Trigger>
-										<Dialog.Portal>
-											<Dialog.Overlay className="menu__overlay" />
-											<Dialog.Content
-												className="menu__content"
-												aria-describedby={undefined}
-											>
-												<Dialog.Title className="sr-only">Menu</Dialog.Title>
-												{/* <Dialog.Description /> */}
-												<Dialog.Close>Close menu</Dialog.Close>
-												<ul className="mt-16 grid gap-6">
-													<li>
-														<MyNavLink to="/">Home</MyNavLink>
-													</li>
-													<li>
-														<MyNavLink to="/nested-routes">
-															Nested routes
-														</MyNavLink>
-													</li>
-												</ul>
-											</Dialog.Content>
-										</Dialog.Portal>
-									</Dialog.Root>
-								)}
-							</nav>
-						</div>
+								</>
+							) : (
+								<Form
+									onSubmit={(e) => {
+										e.preventDefault()
+										console.log('todo: Open menu')
+									}}
+								>
+									<button aria-expanded="false">Menu</button>
+								</Form>
+							)}
+						</nav>
 					</div>
 				</header>
-				<main className="center-3xl">
-					<div className="py-6 tablet:py-20">
-						<Outlet />
-					</div>
+				<main>
+					<Outlet />
 				</main>
 			</div>
 			<ScrollRestoration />
@@ -92,26 +60,14 @@ export function Layout() {
 	)
 }
 
-const navLinkVariants = cva('hocus:underline hocus:underline-offset-4', {
-	variants: {
-		state: { active: 'underline underline-offset-4' },
-	},
-})
-
-interface MyNavLinkProps extends Omit<NavLinkProps, 'className'> {
-	className?: string
+interface DesktopLinkProps {
+	name: string
 }
 
-function MyNavLink({ className, ...props }: MyNavLinkProps) {
+function DesktopLink({ name }: DesktopLinkProps) {
 	return (
-		<NavLink
-			{...props}
-			className={({ isActive }) =>
-				navLinkVariants({
-					className,
-					state: isActive ? 'active' : null,
-				})
-			}
-		/>
+		<li>
+			<Link to="#">{name}</Link>
+		</li>
 	)
 }
