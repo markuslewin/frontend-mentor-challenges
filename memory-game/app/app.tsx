ccd9a2f1ba914b3d1569e504471a200a6e764313
Markus Lewin <lewinmarkus@gmail.com>
2024-10-11 18:27:04 +0200

    Add starter files

--- memory-game/app/app.tsx
+++ memory-game/app/app.tsx
@@ -12,13 +12,16 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { AnnouncementProvider } from '#app/components/announcer'
+import { Icon } from '#app/components/icon'
 
 const iconStyle = { width: 'auto', height: '3.5rem' }
 
 function Memory() {
 	return (
 		<>
-			<h1 className="text-game-title">memory</h1>
+			<h1 className="text-game-title">
+				<Icon name="logo" /> memory
+			</h1>
 			<p className="text-game-meta-current">CURRENT TURN</p>
 			<p className="grid grid-cols-5">
 				<FontAwesomeIcon icon={faFutbolBall} style={iconStyle} />
@@ -43,3 +46,60 @@ export function App() {
 		</AnnouncementProvider>
 	)
 }
+
+// <!-- Game setup start -->
+
+// Select Theme
+// Numbers
+// Icons
+
+// Number of Players
+// 1
+// 2
+// 3
+// 4
+
+// Grid Size
+// 4x4
+// 6x6
+
+// Start Game
+
+// <!-- Game setup end -->
+
+// <!-- Game board start -->
+
+// Restart
+// New Game
+
+// <!-- Multiplayer scores start -->
+
+// Player 1
+// <!-- P1 score -->
+// Current Turn
+
+// Player 2
+// <!-- P2 score -->
+// Current Turn
+
+// Player 3
+// <!-- P3 score -->
+// Current Turn
+
+// Player 4
+// <!-- P4 score -->
+// Current Turn
+
+// <!-- Multiplayer scores start -->
+
+// <!-- Solo game time and moves counter start -->
+
+// Time
+// <!-- Time elapsed -->
+
+// Moves
+// <!-- Moves total -->
+
+// <!-- Solo game time and moves counter end -->
+
+// <!-- Game board end -->
