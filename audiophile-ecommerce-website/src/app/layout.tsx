6df97dccb2b04e4099ba45a2c8f86db895668513
Markus Lewin <lewinmarkus@gmail.com>
2024-11-18 18:51:00 +0100

    Add footer content

--- audiophile-ecommerce-website/src/app/layout.tsx
+++ audiophile-ecommerce-website/src/app/layout.tsx
@@ -1,11 +1,13 @@
 import "~/styles/globals.css";
-
 import { type Metadata } from "next";
 import { Manrope } from "next/font/google";
 import Link from "next/link";
 import { NavLink } from "~/app/_components/nav-link";
 import Logo from "~/app/_assets/logo.svg";
 import IconCart from "~/app/_assets/icon-cart.svg";
+import IconFacebook from "~/app/_assets/icon-facebook.svg";
+import IconInstagram from "~/app/_assets/icon-instagram.svg";
+import IconTwitter from "~/app/_assets/icon-twitter.svg";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -37,7 +39,7 @@ export default function RootLayout({
             <span className="sr-only">Audiophile</span>
           </Link>
           <nav>
-            <ul>
+            <ul role="list">
               {[
                 { name: "Home", href: "/" },
                 { name: "Headphones", href: "/headphones" },
@@ -45,8 +47,13 @@ export default function RootLayout({
                 { name: "Earphones", href: "/earphones" },
               ].map((link, i) => {
                 return (
-                  <li key={i}>
-                    <NavLink href={link.href}>{link.name}</NavLink>
+                  <li className="text-sub-title uppercase" key={i}>
+                    <NavLink
+                      className="transition-colors hocus:text-D87D4A"
+                      href={link.href}
+                    >
+                      {link.name}
+                    </NavLink>
                   </li>
                 );
               })}
@@ -58,19 +65,66 @@ export default function RootLayout({
           </button>
         </header>
         <main>{children}</main>
-        <footer></footer>
+        <footer className="bg-101010 text-FFFFFF">
+          <Link href="/">
+            <Logo className="h-[1.5625rem] w-[8.9375rem]" />
+            <span className="sr-only">Audiophile</span>
+          </Link>
+          <nav>
+            <ul role="list">
+              {[
+                { name: "Home", href: "/" },
+                { name: "Headphones", href: "/headphones" },
+                { name: "Speakers", href: "/speakers" },
+                { name: "Earphones", href: "/earphones" },
+              ].map((link, i) => {
+                return (
+                  <li className="text-sub-title uppercase" key={i}>
+                    <NavLink
+                      className="transition-colors hocus:text-D87D4A"
+                      href={link.href}
+                    >
+                      {link.name}
+                    </NavLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+          <p>
+            Audiophile is an all in one stop to fulfill your audio needs.
+            We&apos;re a small team of music lovers and sound specialists who
+            are devoted to helping you get the most out of personal audio. Come
+            and visit our demo facility - we’re open 7 days a week.
+          </p>
+          <p className="font-bold">Copyright 2021. All Rights Reserved</p>
+          <ul role="list">
+            {[
+              {
+                name: "Facebook",
+                Icon: IconFacebook,
+              },
+              {
+                name: "Twitter",
+                Icon: IconTwitter,
+              },
+              {
+                name: "Instagram",
+                Icon: IconInstagram,
+              },
+            ].map((social, i) => {
+              return (
+                <li key={i}>
+                  <a className="transition-colors hocus:text-D87D4A" href="#">
+                    <social.Icon className="w-6" />
+                    <span className="sr-only">{social.name}</span>
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </footer>
       </body>
     </html>
   );
 }
-
-// Home
-// Headphones
-// Speakers
-// Earphones
-
-// Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers
-// and sound specialists who are devoted to helping you get the most out of personal audio. Come and
-// visit our demo facility - we’re open 7 days a week.
-
-// Copyright 2021. All Rights Reserved
