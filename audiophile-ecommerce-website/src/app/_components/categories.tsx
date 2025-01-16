acb5feea1cfa333eb2bbdd279721777c850e0c23
Markus Lewin <lewinmarkus@gmail.com>
2025-01-16 20:19:34 +0100

    Reimplement categories images

--- audiophile-ecommerce-website/src/app/_components/categories.tsx
+++ audiophile-ecommerce-website/src/app/_components/categories.tsx
@@ -30,18 +30,25 @@ export function Categories({
           {[
             {
               className: "",
+              imageClassName:
+                "w-[8.875rem] desktop:w-[13.625rem] top-0 tablet:top-[0.0625rem]",
+
               image: imageCategoryThumbnailHeadphones,
               name: "Headphones",
               href: "/headphones",
             },
             {
               className: "tablet:col-start-9",
+              imageClassName:
+                "w-[9.375rem] desktop:w-[13.5rem] top-0 desktop:top-[0.4375rem]",
               image: imageCategoryThumbnailSpeakers,
               name: "Speakers",
               href: "/speakers",
             },
             {
               className: "tablet:col-start-[17]",
+              imageClassName:
+                "w-[11.25rem] desktop:w-[13.5rem] top-0 desktop:top-[1.375rem]",
               image: imageCategoryThumbnailEarphones,
               name: "Earphones",
               href: "/earphones",
@@ -49,24 +56,24 @@ export function Categories({
           ].map((category, i) => {
             return (
               <Link
-                className={`${category.className} group relative isolate rounded text-center tablet:col-span-7`}
+                className={`${category.className} group relative isolate rounded pt-[3.25rem] text-center tablet:col-span-7 desktop:pt-20`}
                 key={i}
                 href={category.href}
                 onClick={onSelect}
               >
-                <div className="aspect-[350/80] w-full" />
-                <div className="rounded-inherit bg-F1F1F1 pb-6">
-                  <div className="aspect-[350/130] w-full" />
-                  <div className="absolute inset-x-0 top-0 mx-auto grid aspect-[218/210] w-[62%] items-end">
-                    <Image className="w-full" alt="" src={category.image} />
-                  </div>
-                  <div className="relative grid justify-items-center">
-                    <CategoryHeading className="-mt-4 text-h6 text-000000">
+                <div className="rounded-inherit bg-F1F1F1 pb-6 pt-[5.5rem] desktop:pt-[7.25rem]">
+                  <Image
+                    className={`${category.imageClassName} absolute inset-x-0 mx-auto`}
+                    alt=""
+                    src={category.image}
+                  />
+                  <div className="relative grid text-center">
+                    <CategoryHeading className="text-h6 text-000000">
                       {category.name}
                     </CategoryHeading>
-                    <p className="mt-4">
+                    <p className="mt-4 text-sub-title uppercase">
                       <span
-                        className="flex items-center gap-3 text-sub-title uppercase transition-colors group-hocus:text-D87D4A"
+                        className="inline-flex items-center gap-3 transition-colors group-hocus:text-D87D4A"
                         aria-hidden="true"
                       >
                         Shop{" "}
